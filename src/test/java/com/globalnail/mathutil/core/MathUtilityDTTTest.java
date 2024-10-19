2b59423120aff7841a4db9344f62b842e783a6b8
Khanh Tien <nthnamto147@gmail.com>
2024-10-19 08:46:56 +0700

    update user v4 ci-script, undo DDT

--- src/test/java/com/globalnail/mathutil/core/MathUtilityDTTTest.java
+++ src/test/java/com/globalnail/mathutil/core/MathUtilityDTTTest.java
@@ -25,7 +25,7 @@ class MathUtilityDTTTest {
                     {3, 6},
                     {4, 24},
                     {5, 120},
-                    {6, 7200},
+                    {6, 720},
                 };
     }
     @ParameterizedTest
